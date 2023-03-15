b4ca32349467e4dbfa1809bf3775fdeabf9e1342
Tho Pham (Alex) <20041501+thopd88@users.noreply.github.com>
2023-03-15 18:18:32 +0700

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php # Script 3.7 - index.php #2
+
+// This function outputs theoretical HTML
+// for adding ads to a Web page.
+function create_ad() {
+  echo '<div class="alert alert-info" role="alert"><p>This is an annoying ad! This is an annoying ad! This is an annoying ad! This is an annoying ad!</p></div>';
+} // End of the function definition.
+
+$page_title = 'Welcome to this Site!';
+include('includes/header.html');
+
+// Call the function:
+// create_ad();
+?>
+
+<div class="page-header"><h1>Content Header</h1></div>
+<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+<?php
+// Call the function again:
+// create_ad();
+
+include('includes/footer.html');
+?>
